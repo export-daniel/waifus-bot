4ed6e3c2eba50c01a4b89bb9d6ad5b52fc3a8219
Daniel <116016343+export-daniel@users.noreply.github.com>
2024-02-10 23:09:19 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,122 @@
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone'
+import { en, es, id, ar, pt } from './lib/idiomas/total-idiomas.js'
+
+global.owner = [
+['595973755967', 'Bot OFC', true], //il.ẉaifus.bot.il
+['595983799436', 'Owner', true]] //il.ẉaifus.albums.il
+global.mods = [] 
+global.prems = []
+
+global.lenguajeBot = es
+
+// ES ➜ Consigue Apikey en https://platform.openai.com/account/api-keys
+global.openai_key = 'sk-0'
+
+// ES ➜ Consigue tu ID de organizacion en: https://platform.openai.com/account/org-settings
+global.openai_org_id = 'org-3'
+
+global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f']
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.lolkeysapi = 'GataDios'
+global.itsrose = ['4b146102c4d500809da9d1ff']
+global.baileys = '@whiskeysockets/baileys'
+
+global.APIs = { 
+  xteam: 'https://api.xteam.xyz',
+  dzx: 'https://api.dhamzxploit.my.id',
+  lol: 'https://api.lolhuman.xyz',
+  violetics: 'https://violetics.pw',
+  neoxr: 'https://api.neoxr.my.id',
+  zenzapis: 'https://api.zahwazein.xyz',
+  akuari: 'https://api.akuari.my.id',
+  akuari2: 'https://apimu.my.id',	
+  fgmods: 'https://api-fgmods.ddns.net',
+  botcahx: 'https://api.botcahx.biz.id',
+  ibeng: 'https://api.ibeng.tech/docs',	
+  rose: 'https://api.itsrose.site',
+  popcat : 'https://api.popcat.xyz',
+  xcoders : 'https://api-xcoders.site'
+},
+global.APIKeys = { 
+  'https://api.xteam.xyz': `${keysxteam}`,
+  'https://api.lolhuman.xyz': `${lolkeysapi}`,
+  'https://api.neoxr.my.id': `${keysneoxr}`,	
+  'https://violetics.pw': 'beta',
+  'https://api.zahwazein.xyz': `${keysxxx}`,
+  'https://api-fgmods.ddns.net': 'fg-dylux',
+  'https://api.botcahx.biz.id': 'Admin',
+  'https://api.ibeng.tech/docs': 'tamvan',
+  'https://api.itsrose.site': 'Rs-Zeltoria',
+  'https://api-xcoders.site': 'Frieren'
+}
+
+global.cheerio = cheerio
+global.fs = fs
+global.fetch = fetch
+global.axios = axios
+global.moment = moment	
+
+global.packname = 'il.ẉaifus.bot.il'
+global.author = `${vs}`
+
+// ES ➜ Está parte es para mostrar el contacto de alguien al usar #contacto
+// EN ➜ This part is to display someone's contact using #contact
+global.official = [ 
+['18059196237', 'Gata Dios', 1], 
+['595983799436', 'il.ẉaifus.albums.il', 1]] 
+
+global.mail = '' //agrega tú correo
+global.desc = '' //agrega una descripción corta
+global.desc2 = '' //agrega una descripción larga (Solo se aplicará si su whasapp no tiene descripción)
+global.country = '🇵🇾' //agrega tú país ejemplo: 🇪🇨
+
+global.vs = '1.0.0-(beta)'
+global.vsJB = '4.5 (Beta)'
+
+global.bot = 'il.ẉaifus.bot.il'
+global.yt = 'https://www.youtube.com'
+global.ig = 'https://www.instagram.com'
+global.md = 'https://www.github.com'
+global.fb = 'https://www.facebook.com'
+global.asistencia = 'wa.me/595983799436' //il.ẉaifus.albums.il
+
+global.wm = 'il.ẉaifus.bot.il'
+global.nomorown = owner[0][0]
+
+global.img = fs.readFileSync('./media/menus/img.jpg')
+global.img1 = fs.readFileSync('./media/menus/img1.jpg')
+global.img2 = fs.readFileSync('./media/menus/img2.jpg')
+global.botlinks = [ ig, md, yt, fb , bot]
+global.botimg = [img, img1, img2]
+
+global.htki = '*⭑•̩̩͙⊱•••• ☪*'
+global.htka = '*☪ ••••̩̩͙⊰•⭑*'
+global.htjava = '⫹⫺'
+global.correct = '✅'
+global.fault = '💔'
+global.alert = '⚠️'
+global.sending = '📋'
+global.sent = '❇️'
+global.notsent = '❗'
+global.waitemot = '⌛'
+global.waitemot2 = '⏳'
+
+global.multiplier = 600 // Cuanto más alto, más difícil subir de nivel 
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+unwatchFile(file)
+console.log(chalk.bold.greenBright(lenguajeGB['smsConfigBot']().trim()))
+import(`${file}?update=${Date.now()}`)
+})
